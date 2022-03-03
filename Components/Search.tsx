add918200de22fe8c84602a67484e56419dbb399
Liam park <broheat@kakao.com>
2022-03-03 16:33:05 +0900

    next, tailwind setting

--- Components/Search.tsx
+++ Components/Search.tsx
@@ -1,3 +1,37 @@
 export default function Search() {
-  return <div>search</div>;
-}
+  return (<section>
+<div className="search">
+  <div className="icon"></div>
+</div>
+<form >
+  <div className="row">
+    <div className="option">
+      <select name="" id="">
+        <option value="">지역</option>
+        <option value="">부산</option>
+        <option value="">OPTION3</option>
+        <option value="">OPTION4</option>
+      </select>
+      <select name="" id="">
+        <option value="">도시</option>
+        <option value="">부산</option>
+        <option value="">OPTION3</option>
+        <option value="">OPTION4</option>
+      </select>
+      <select name="" id="">
+        <option value="">종류</option>
+        <option value="">부산</option>
+        <option value="">OPTION3</option>
+        <option value="">OPTION4</option>
+      </select>
+      <select name="" id="">
+        <option value="">메인 음식</option>
+        <option value="">부산</option>
+        <option value="">OPTION3</option>
+        <option value="">OPTION4</option>
+      </select>
+    </div>
+  </div>
+</form>
+  </section>
+  )}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Search from "../Components/Search"
 const Home: NextPage = () => {
   return (
     <div>
+      <header className='text-3xl font-bold underline'>함께 만들어가는 맛집 지도</header>
       <Search/>
     </div>
   )
